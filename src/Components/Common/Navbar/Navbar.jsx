fedd180f57c498c52753fd0b19af99e7f6bf4bb6
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2025-03-29 17:42:55 +0330

    Edit navbar style

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Menu from "./Menu";
 import Logo from "./Logo";
-import DarkmodeNotif from './Darkmode&Notif';
+import DarkmodeNotif from "./Darkmode&Notif";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex h-14 justify-between">
-     <Logo />
-     <Menu />
-     <DarkmodeNotif />
+    <div className="w-full flex h-14 justify-between m-1">
+      <Logo />
+      <Menu />
+      <DarkmodeNotif />
     </div>
   );
 };
