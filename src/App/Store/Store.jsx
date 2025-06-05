d9bc4d87eadfd146a903be1eecc715629e1fdb1f
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2025-06-05 14:52:21 +0330

    feat(auth): persist auth state with redux-persist to prevent logout on refresh

--- src/App/Store/Store.jsx
+++ src/App/Store/Store.jsx
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../../Components/Features/Auth/AuthSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const Store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(Store);
